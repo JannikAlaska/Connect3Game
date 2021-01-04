0c705e8e622ff12ddcc5ff18b814f76586a699a9
JannikAlaska <jannik@berthold-ilsenburg.de>
2021-01-04 17:35:12 +0100

    board as image imported ImageViews created

--- app/src/main/java/berthold/berthold/connect3game/MainActivity.java
+++ app/src/main/java/berthold/berthold/connect3game/MainActivity.java
@@ -4,9 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean playerOnesTurn = true;
+
+    public void dropIn(View view){
+
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1500);
+        counter.setImageResource(R.drawable.yellow);
+        counter.animate().translationYBy(1500).setDuration(300);
+
+        }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
