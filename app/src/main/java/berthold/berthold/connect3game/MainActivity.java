ad16bb90c5bfd429f41b9e07153a3ab571c698c0
JannikAlaska <jannik_berthold@web.de>
2021-01-04 23:18:22 +0100

    Change Colour & dropIn added

--- app/src/main/java/berthold/berthold/connect3game/MainActivity.java
+++ app/src/main/java/berthold/berthold/connect3game/MainActivity.java
@@ -2,32 +2,43 @@ package berthold.berthold.connect3game;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    boolean redsTurn = true;
+    int activePlayer = 1;
 
     public void dropIn (View view) {
+
+        Log.i("Info:", view.getTag().toString());
+        int i = Integer.parseInt(view.getTag().toString());
+
+        int[] gameState [2,2,2,2,2,2,2,2,2]
 
 
         ImageView counter = (ImageView) view;
 
         counter.setTranslationY(-1500);
 
+        //array in if-Statements nicht möglich
+        //Prüfung, ob Feld schon belegt ist, um nicht mehrfach auswählbar zu sein
+        if (gameState[i]==2) {
 
-        if (redsTurn) {
+        }
+        if (activePlayer == 1 + gameState[i] = 2) {
 
             counter.setImageResource(R.drawable.red);
-            redsTurn = false;
+            activePlayer = 0;
 
         } else {
 
             counter.setImageResource(R.drawable.yellow);
-            redsTurn = true;
+            activePlayer = 1;
 
         }
 
