0f9f411f45d0145917f17adbaf0c69fc962d179b
JannikAlaska <jannik_berthold@web.de>
2021-01-04 22:17:59 +0100

    Change Colour & dropIn added

--- app/src/main/java/berthold/berthold/connect3game/MainActivity.java
+++ app/src/main/java/berthold/berthold/connect3game/MainActivity.java
@@ -4,8 +4,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    boolean redsTurn = true;
+
+    public void dropIn (View view) {
+
+
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1500);
+
+
+        if (redsTurn) {
+
+            counter.setImageResource(R.drawable.red);
+            redsTurn = false;
+
+        } else {
+
+            counter.setImageResource(R.drawable.yellow);
+            redsTurn = true;
+
+        }
+
+
+        counter.animate().translationYBy(1500).setDuration(100);
+    }
 
 
     @Override
